a5cc0b53b011f3ac74888db01e796b8fba47b27e
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-04-03 14:53:53 +0530

    final

--- geektrust.js
+++ geektrust.js
@@ -1,22 +1,18 @@
-
-
-
-
-async function getData(){
+async function getList(){
     let res = await fetch(`https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json`)
 
-    let data = await res.json()
+    let myList = await res.json()
 
-    showData(data)
+    showList(myList)
 
-    console.log(data)
+    console.log(myList)
 
 }
-getData()
+getList()
 
 
-function showData(data){
-    data.map((e)=>{
+function showList(myList){
+    myList.map((e)=>{
         let div = document.createElement('div')
 
         let p1 = document.createElement('p');
@@ -35,6 +31,47 @@ function showData(data){
 }
 
 
+
 const searchFun = () => {
-    let filter = document.getElementById("myInput").ariaValueMax.toUpperCase();
+
+    let filter = document.getElementById('myInput').value.toUpperCase();
+
+    let myTable = document.getElementById('myTable');
+    
+    let tr = myTable.getElementsByTagName('tr');
+
+    for(var i=0; i<tr.length; i++) {
+        let td = tr[i].getElementsByTagName('td')[0];
+
+        if(td){
+            let textvalue = td.textContent || td.innerHTML;
+
+            if(textvalue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+    
+function showList(data){
+    var table = document.getElementById('myTable')
+    table.innerHTML = ''
+
+
+    for (var i = 0; i < data.length; i++){
+        var colname = `name-${i}`
+        var colemail = `email-${i}`
+        var colrole = `role-${i}`
+
+        var row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].email}</td>
+                        <td>${data[i].role}</td>
+                   </tr>`
+        table.innerHTML += row
+    }
 }
